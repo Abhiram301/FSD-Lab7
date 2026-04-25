40b68027ce1a56a9f8093294a3224b8b0df378f7
Abhiram301 <abhirambhagath10@gmail.com>
2026-04-25 12:04:18 +0530

    Update lab code

--- LAB7/src/components/ARScene.jsx
+++ LAB7/src/components/ARScene.jsx
@@ -4,13 +4,14 @@ const ARScene = () => {
   return (
     <a-scene vr-mode-ui="enabled: true">
 
-      {/* Assets */}
-      <a-assets>
-        <img id="classroom" src="/classroom.jpg" alt="classroom background" crossOrigin="anonymous" />
-      </a-assets>
+      {/* 360-degree sky - use a solid color until classroom.jpg is added */}
+      <a-sky color="#87CEEB"></a-sky>
 
-      {/* 360-degree classroom */}
-      <a-sky src="#classroom"></a-sky>
+      {/* AR box from Step 4 */}
+      <a-box id="box" position="0 0 -3" color="#4CC3D9" scale="1 1 1"></a-box>
+
+      {/* Ground */}
+      <a-plane position="0 0 -4" rotation="-90 0 0" width="10" height="10" color="#7BC8A4"></a-plane>
 
       {/* Camera at seated position */}
       <a-entity
